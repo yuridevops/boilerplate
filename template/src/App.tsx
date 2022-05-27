21763879e133b85c0ca320e0d35ff4c0d9ba1557
Marcos Yuri <yuri.devops@gmail.com>
2022-05-27 13:56:32 -0400

    fix: general fixes

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '~assets/styles/global'
 import theme from '@frete.com/fuel-foundation/fretebras/theme.json'
+import truck from '~assets/images/truck.gif'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div>Boilerplate content</div>
+      <img
+        src={truck}
+        style={{ height: 200, position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)' }}
+      />
     </ThemeProvider>
   )
 }
